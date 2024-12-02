628e3a4a857fa76e6838b3639180cf80a5042ff7
Pase Temitope <pasetope@gmail.com>
2024-12-02 19:57:23 +0000

    comments

--- src/main/java/finalCarTune/CARTUNE/Model/User.java
+++ src/main/java/finalCarTune/CARTUNE/Model/User.java
@@ -1,11 +1,23 @@
 package finalCarTune.CARTUNE.Model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Data;
 
 @Entity
 @Data
@@ -44,6 +56,7 @@ public class User {
         super();
     }
 
+    // for constructor
     public User(String firstName, String lastName, String email, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -51,10 +64,12 @@ public class User {
         this.password = password;
     }
 
+    // to add role
     public void addRole(Role role) {
         this.roles.add(role);
     }
 
+    // to remove role
     public void removeRole(Role role) {
         this.roles.remove(role);
     }
